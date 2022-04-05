928e2f579d15b9a65326e743a5e5dcc10fa8bdc4
Amovane <amovane@163.com>
2022-04-06 03:49:01 +0800

    Add script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,25 @@
+import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 export default function FirstPost() {
   return (
-      <>
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      ></Script>
       <h1>First Post</h1>
       <h2>
-      <Link href={"/"}>
+        <Link href={"/"}>
           <a>Back to home</a>
-      </Link>
+        </Link>
       </h2>
-      </>
-  )
+    </>
+  );
 }
