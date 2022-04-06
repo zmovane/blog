08ea7335c33866f3d7ff1f5899ed794e93ea01d6
Amovane <amovane@163.com>
2022-04-06 16:05:46 +0800

    Format date string

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
@@ -28,7 +30,7 @@ export default function Post({ postData }) {
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
