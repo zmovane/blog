594bbdc3fb03d498374ae943f1a5a651ec7eb76e
Amovane <amovane@163.com>
2022-04-06 10:45:35 +0800

    Add components/layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
 import Script from "next/script";
+import Layout from "../../components/layout";
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -20,6 +21,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
