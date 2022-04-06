47b72d46aac8fcea33401d61f5dedf6875fd9f38
Amovane <amovane@163.com>
2022-04-06 16:02:34 +0800

    Render posts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -11,11 +21,32 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>
           Hello, I'm Amovane, a software engineer from china. I'm learning
-          blockchain technology in recent time. I've made some opensource projects on <a href="https://github.com/amovane" target="_blank"> Amovane's Github</a>.
+          blockchain technology in recent time. I've made some opensource
+          projects on{" "}
+          <a href="https://github.com/amovane" target="_blank">
+            {" "}
+            Amovane's Github
+          </a>
+          .
         </p>
         <p>
           This's my blog, I'll share some personal experiences about programing.
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
